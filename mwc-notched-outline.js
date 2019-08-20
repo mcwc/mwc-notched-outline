2c8fd31f871d8b547d2433748c3c0a8dd5bbf1fc
teste <teste@teste.teste>
2019-08-20 16:57:58 -0300

    mcwc

---
+++ mwc-notched-outline.js
@@ -0,0 +1,33 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+/**
+@license
+Copyright 2019 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { customElement } from '@material/mwc-base/form-element.js';
+import { NotchedOutlineBase } from './mwc-notched-outline-base.js';
+import { style } from './mwc-notched-outline-css.js';
+let NotchedOutline = class NotchedOutline extends NotchedOutlineBase {
+};
+NotchedOutline.styles = style;
+NotchedOutline = __decorate([
+    customElement('mwc-notched-outline')
+], NotchedOutline);
+export { NotchedOutline };
+//# sourceMappingURL=mwc-notched-outline.js.map
